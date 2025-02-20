4c3c25207e63285b06f0346c9f70ea996686c146
Roberto Sánchez <roberto.sanchez2024@hilahasl.com>
2025-02-20 09:26:00 -0600

    :sparkles: First change

---
+++ script.js
@@ -0,0 +1,5 @@
+    let contenedor = document.getElementById("contenido");
+    let nuevoTitulo = document.createElement("H2")
+    nuevoTitulo.textContent = "Manual de Prácticas";
+    contenedor.appendChild(nuevoTitulo);
+
